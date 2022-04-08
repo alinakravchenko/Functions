8195990585a6e02dd1df365d56be38571ee4bd3c
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-04-08 21:21:55 +0300

    Functions day2

--- Functions/Source.cpp
+++ Functions/Source.cpp
@@ -6,18 +6,24 @@ int Add(int a, int b); //прототип функции(объ€вление функции-Function declaratio
 int Sub(int a, int b);//прототип состоит только из заголовка функции.
 int Mul(int a, int b/*принимаемые параметры*/);
 double Div(double a, double b);
+float Power(float  a, float b);
 //                   UNRESOLVE EXTERNALS
 void main()
 {
 	setlocale(LC_ALL, "");
 	//арифмет. операции над числами с помощью функции
 	int a, b;
+	float n, m;
 	cout << "введите два числа: "; cin >> a >> b;
 	int c = Add(a, b);//вызов (использование) функции 
 	cout << a << "+" << b << "=" << c << endl;
 	cout << a << "-" << b << "=" << Sub(a, b) << endl;
 	cout << a << "*" << b << "=" << Mul(a, b/*передаваемые параметры*/) << endl;
 	cout << a << "/" << b << "=" << Div(a, b) << endl;
+	cout << "¬ведите число: "; cin >> n;
+	cout << "¬ведите степень в которую нужно возвести число: ";
+	cin >> m;
+	cout << n << " в степени " << m << " равно "<< pow(n,m);
 }
 int Add(int a, int b) //реализаци€ функции (определение функции-function definition)
 //реализует тот алгоритм, за который ответственна функци€ (то, что делает функци€).
@@ -36,4 +42,9 @@ int Mul(int a, int b)
 double Div(double a, double b)
 {
 	return a / b;
+}
+
+float Power(float a, float b)
+{
+	return pow(a, b);
 }
